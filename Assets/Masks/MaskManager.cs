1a1b627b3af51dc7f4ffd3b9db2660211ad537a2
AmyFoxie <dragonhel5@gmail.com>
2022-03-23 01:42:03 +0100

    Work on mask

---
+++ Assets/Masks/MaskManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskManager : MonoBehaviour {
+    public List<Mask> masks;
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
+
+public interface Mask {
+    public void useAbility();
+}
+
+public class FoxMask : Mask {
+    public void useAbility () {
+        
+    }
+}
