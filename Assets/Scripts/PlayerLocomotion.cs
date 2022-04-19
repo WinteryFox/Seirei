f0a4c4adcb395fc8138feb9dc7ec35ea7506f7b4
marinastancu <448358@student.fontys.nl>
2022-04-19 15:04:09 +0200

    remade the kitsune mask, also created a rabbit one; both are now imported into the project's assets

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -58,7 +58,7 @@ public class PlayerLocomotion : MonoBehaviour
             return;
         HandleMovement();
         HandleRotation();
-        HandleStairclimb();
+        //HandleStairclimb();
     }
 
     public void HandleMovement()
@@ -180,40 +180,40 @@ public class PlayerLocomotion : MonoBehaviour
         }
     }
 
-    public void HandleStairclimb()
-    {
-        RaycastHit hitLower;
-        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, 0.1f))
-        {
-            RaycastHit hitUpper;
-            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, 0.2f))
-            {
-                playerRigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
-            }
-        }
+    //public void HandleStairclimb()
+    //{
+    //    RaycastHit hitLower;
+    //    if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, 0.1f))
+    //    {
+    //        RaycastHit hitUpper;
+    //        if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, 0.2f))
+    //        {
+    //            playerRigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+    //        }
+    //    }
 
-        RaycastHit hitLower45;
-        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, 0.1f))
-        {
+    //    RaycastHit hitLower45;
+    //    if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, 0.1f))
+    //    {
 
-            RaycastHit hitUpper45;
-            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitUpper45, 0.2f))
-            {
-                playerRigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
-            }
-        }
+    //        RaycastHit hitUpper45;
+    //        if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitUpper45, 0.2f))
+    //        {
+    //            playerRigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+    //        }
+    //    }
 
-        RaycastHit hitLowerMinus45;
-        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.1f))
-        {
+    //    RaycastHit hitLowerMinus45;
+    //    if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.1f))
+    //    {
 
-            RaycastHit hitUpperMinus45;
-            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, 0.2f))
-            {
-                playerRigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
-            }
-        }
-    }
+    //        RaycastHit hitUpperMinus45;
+    //        if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, 0.2f))
+    //        {
+    //            playerRigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+    //        }
+    //    }
+    //}
 
     public void PlayFootstepSound()
     {
