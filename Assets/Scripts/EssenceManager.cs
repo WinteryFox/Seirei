7d547c3111baf604a6c458b8435a2fc04b879ce5
AmyFoxie <dragonhel5@gmail.com>
2022-04-21 17:14:01 +0200

    Progress bar for spirit essence

--- Assets/Scripts/EssenceManager.cs
+++ Assets/Scripts/EssenceManager.cs
@@ -1,9 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class EssenceManager : MonoBehaviour {
+    public Image fill;
+    private List<GameObject> usedShrines = new();
     private Camera playerCamera;
     private PlayerControls playerControls;
     private int essence;
@@ -16,26 +18,26 @@ public class EssenceManager : MonoBehaviour {
         playerControls.PlayerActions.Use.canceled += _ => useInput = false;
         playerControls.Enable();
     }
-    
-    void Update() {
+
+    void Update () {
         if (!useInput)
             return;
-        
+
         var ray = playerCamera.ScreenPointToRay (Mouse.current.position.ReadValue());
         if (!Physics.Raycast (ray, out var hit))
             return;
 
         var selection = hit.transform.gameObject;
         var shrine = selection.GetComponent<Shrine>();
-        if (shrine == null)
+        if (shrine == null || usedShrines.Contains (selection))
             return;
 
-        var t = playerCamera.transform;
+        /*var t = playerCamera.transform;
         var originalCameraPosition = t.localPosition;
         var shrinePosition = shrine.transform.position;
-        t.localPosition = shrinePosition + new Vector3 (-5f, 0f, 0f);
+        t.localPosition = shrinePosition + new Vector3 (-5f, 0f, 0f);*/
         // TODO: Play cool animation and some mysterious music
-        essence += 100;
-        // TODO: Reset camera to original position
+        fill.fillAmount += 0.5f;
+        usedShrines.Add (selection);
     }
 }
